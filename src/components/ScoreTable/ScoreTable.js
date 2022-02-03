bc020560fbc652d0d9cc2a8b6ecfd573acdc7503
mikiyashussen <mikiyashussen19@gmail.com>
2022-02-03 03:29:37 +0100

    Test 2

--- src/components/ScoreTable/ScoreTable.js
+++ src/components/ScoreTable/ScoreTable.js
@@ -1,24 +1,58 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './ScoreTable.css'
 
 const ScoreTable = ({tableData}) => {
-  
+    let classNameHome = ''
+    let classNameAway = ''
+
+    const assignClassName = (match) => {
+     let matchResult = match.sport_event_status.match_tie ? 'tie' : match.sport_event_status.winner_id;
+    if(matchResult === 'tie'){
+      classNameHome='tie';
+      classNameAway='tie';
+    }
+    else if(matchResult === match.sport_event.competitors[0].id){
+        classNameHome='winner';
+        classNameAway='loser';
+    }
+    else {
+      classNameHome='loser';
+      classNameAway='winer';
+    }
+  }
+
   const createRow = (match,index) => {
-    
+    assignClassName(match)
     return (
             <tr >
-              <td>{`${match.sport_event.competitors[0].name } - ${match.sport_event.competitors[1].name}`}</td>
-              <td>{match.sport_event_status.home_score} {'-'} {match.sport_event_status.away_score}</td>
+                <td className={classNameHome} 
+                style={{backgroundColor: classNameHome === 'winner' ? 'green' :
+                 (classNameHome === 'loser' ? 'red' : 'orange')}}
+                >{match.sport_event.competitors[0].name } </td>
+                <td className={classNameAway}
+                style={{backgroundColor: classNameAway === 'winner' ? 'green' :
+                 (classNameAway === 'loser' ? 'red' : 'orange')}}>{match.sport_event.competitors[1].name}</td>
+                <td>{match.sport_event_status.home_score} {'-'} {match.sport_event_status.away_score}</td>
+                {/* <td>{match.sport_event_status.period_scores[0].away_score} {'-'} {match.sport_event_status.period_scores[0].away_score}</td>  */}
+                <td>{tableData.schedules[1].sport_event_status.period_scores[0].home_score}{'-'}{tableData.schedules[1].sport_event_status.period_scores[0].away_score}</td>
+                <td>{`${(match.sport_event.start_time).substring(0,10)}  ${(match.sport_event.start_time).substring(12,16)}`}</td>
+                <td>{match.sport_event.venue.name}</td>
             </tr>
           )
   }
   return <div>
-    <Table className='scoreTable'  bordered hover variant="dark">
+    <Table className='scoreTable'  bordered  variant="dark">
       <thead>
         <tr>
-          <th>Home</th>
+          <th>Home Team</th>
+          <th>Away Team</th>
           <th>Score</th>
+          <th>Half-time Score</th>
+          <th>Date</th>
+          <th>Stadium</th>
+
         </tr>
       </thead>
         {tableData.schedules && 
